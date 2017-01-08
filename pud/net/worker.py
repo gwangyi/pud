d4c1377651f748e6bcd45a888afe9f804394cf8f
Sungkwang Lee <gwangyi.kr@gmail.com>
2017-01-08 15:24:28 +0900

    Logging feature updated & protocol pre-loading added

--- pud/net/worker.py
+++ pud/net/worker.py
@@ -1,6 +1,9 @@
+import sys
+import logbook.queues
 import optparse
 import gevent
 import zerorpc
+import platform
 from . import discovery
 from ..interface import Device, available_protocols, load_protocol
 
@@ -59,24 +62,43 @@ class WorkerService:
 
 
 def main():
-
     parser = optparse.OptionParser()
     parser.add_option("-s", "--xsub", dest="xsub", help="Master's subscriber endpoint", action="append")
+    parser.add_option("-e", "--endpoint", dest="endpoint", help="Worker rpc endpoint", action="append")
+    parser.add_option("-p", "--protocol", dest="protocol", help="Specifying pre-loading protocol", action="append")
+    parser.add_option("-l", "--available-protocols", dest="available", help="List all available protocols", action="store_true")
+    parser.add_option("-q", "--quite", dest="quite", help="Turn off stdout log output", action="store_true")
+    parser.add_option("-z", "--zeromq-log", dest="zmqlog", help="Specifying ZeroMQ log subscriber", action="append")
 
     options, args = parser.parse_args()
-    if options.xsub is None or len(args) == 0:
+    if options.available:
+        w = WorkerService()
+        for protocol in w.available_protocols():
+            print(protocol)
+        exit(0)
+    if options.xsub is None or options.endpoint is None:
         parser.print_help()
         exit(2)
+    if options.protocol is None:
+        options.protocol = []
+    if not options.quite:
+        logbook.StreamHandler(sys.stdout, bubble=True).push_application()
+    if options.zmqlog:
+        logbook.Processor(lambda r: r.extra.__setitem__('from', platform.node())).push_application()
+        for ep in options.zmqlog:
+            logbook.queues.ZeroMQHandler(ep, multi=True, bubble=True).push_application()
 
     d = discovery.DiscoveryService()
     d.add_subs(*options.xsub)
     disc_job = gevent.spawn(d.run)
 
-    s = zerorpc.Server(WorkerService())
-    for ep in args:
+    w = WorkerService()
+    w.load_protocol(*options.protocol)
+    s = zerorpc.Server(w)
+    for ep in options.endpoint:
         s.bind(ep)
     d.add_tags("pud-worker")
-    d.add_endpoints(*args)
+    d.add_endpoints(*options.endpoint)
     gevent.joinall([
         disc_job,
         gevent.spawn(s.run)
